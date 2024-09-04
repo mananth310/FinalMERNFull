588c5a591a1f314f80ca5fda4193b4ee7c05b081
ananthvcewacin <ananth@vcew.ac.in>
2024-09-04 19:00:09 +0530

    ananth_update

---
+++ server/model/student.js
@@ -0,0 +1,19 @@
+const mongoose =require("mongoose");
+const StudentSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    cno:String,
+    dob:Date,
+    course:String,
+    status:{
+        type:Number,
+        default:0
+    },
+    batch:{
+        type:String,
+        default:"NA"
+    }
+
+});
+const StudentModel=mongoose.model("student",StudentSchema)
+module.exports=StudentModel
